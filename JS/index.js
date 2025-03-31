3ecbaf69bb19e2cdd57b1fcf76a84c37517a1ee3
Lucas pasnon <lucas.pasnon@proton.me>
2025-03-31 21:41:00 +0200

    first commit

--- JS/index.js
+++ JS/index.js
@@ -20,7 +20,7 @@ function startScanner() {
         },
       },
       decoder: {
-        readers: ["code_128_reader", "ean_reader", "upc_reader"], // Formats de codes-barres à scanner
+        readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"],
       },
     },
     (err) => {
